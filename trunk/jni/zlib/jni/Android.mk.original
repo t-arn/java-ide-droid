84c85c3ea6522b7a08dedc59cc3b06be6c1f5f97
tom.arn66@gmail.com <tom.arn66@gmail.com@e8f23bdd-fea3-abd2-21fc-4596e358f515>
2011-07-25 23:14:55 +0000

    more jni sources

---
+++ trunk/jni/zlib/jni/Android.mk.original
@@ -0,0 +1,92 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# measurements show that the ARM version of ZLib is about x1.17 faster
+# than the thumb one...
+LOCAL_ARM_MODE := arm
+
+zlib_files := \
+	adler32.c \
+	compress.c \
+	crc32.c \
+	gzclose.c \
+	gzlib.c \
+	gzread.c \
+	gzwrite.c \
+	uncompr.c \
+	deflate.c \
+	trees.c \
+	zutil.c \
+	inflate.c \
+	infback.c \
+	inftrees.c \
+	inffast.c
+
+LOCAL_MODULE := libz
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_SRC_FILES := $(zlib_files)
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+LOCAL_MODULE := libz
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_SRC_FILES := $(zlib_files)
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+LOCAL_MODULE := libz
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -O3 -DUSE_MMAP
+LOCAL_SRC_FILES := $(zlib_files)
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+
+
+unzip_files := \
+	adler32.c \
+	crc32.c \
+	zutil.c \
+	inflate.c \
+	inftrees.c \
+	inffast.c
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(unzip_files)
+LOCAL_MODULE:= libunz
+LOCAL_ARM_MODE := arm
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(unzip_files)
+LOCAL_MODULE:= libunz
+LOCAL_ARM_MODE := arm
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=        \
+	minigzip.c
+
+LOCAL_MODULE:= gzip
+
+LOCAL_SHARED_LIBRARIES := libz
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=        \
+	minigzip.c
+
+LOCAL_MODULE:= minigzip
+
+LOCAL_STATIC_LIBRARIES := libz
+
+include $(BUILD_HOST_EXECUTABLE)
+
---
+++ trunk/jni/zlib/obj/local/armeabi/objs/z/gzclose.o.d
@@ -0,0 +1,152 @@
+obj/local/armeabi/objs/z/gzclose.o: jni/gzclose.c jni/gzguts.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ jni/zlib.h jni/zconf.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/signal.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/limits.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/string.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/errno.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h
+
+jni/gzguts.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+jni/zlib.h:
+
+jni/zconf.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/string.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/cygdrive/u/Programs/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
