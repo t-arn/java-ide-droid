e39c17edb255e567a74553a0cd6fe216147563c1
tom.arn66 <tom.arn66@gmail.com>
2011-08-08 20:06:13 +0000

    Version 0.7.2

--- src/com/t_arn/JavaIDEdroid/SettingActivity.java
+++ src/com/t_arn/JavaIDEdroid/SettingActivity.java
@@ -1,6 +1,7 @@
 package com.t_arn.JavaIDEdroid;
 
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
@@ -15,8 +16,8 @@ import android.util.TypedValue;
 public class SettingActivity extends PreferenceActivity 
 //##################################################################
 {
-  private static String stFontSize;
-  private static String stFontType;
+  public static String stFontSize;
+  public static String stFontType;
 
 //===================================================================
   /** Called when the activity is first created. */
@@ -28,11 +29,18 @@ public class SettingActivity extends PreferenceActivity
     this.addPreferencesFromResource(R.xml.settings);
   }
 //===================================================================
+  @Override 
+  public void onConfigurationChanged(Configuration config)
+//===================================================================
+  {
+    super.onConfigurationChanged(config);
+  }
+//===================================================================
 /** Load, parse and apply the settings
  *
  * @param main the main activity
  */
-  public static void fnApplySettings(MainActivity main)
+  public void fnApplySettings()
 //===================================================================
 {
    final SharedPreferences prefs;
@@ -43,34 +51,34 @@ public class SettingActivity extends PreferenceActivity
    try { fFontSize = Float.parseFloat(stFontSize); } catch (NumberFormatException e1) { fFontSize=12; }
 
    // load the preferences
-   prefs = PreferenceManager.getDefaultSharedPreferences(main);
+   prefs = PreferenceManager.getDefaultSharedPreferences(G.main);
    try { stFontSize = prefs.getString("font_size", stFontSize); }
-   catch (ClassCastException e2) { Log.e(MainActivity.stProgramName, "ClassCastException on getting 'font_size'"); }
+   catch (ClassCastException e2) { Log.e(G.stProgramName, "ClassCastException on getting 'font_size'"); }
    try { stFontType = prefs.getString("font_type",stFontType); }
-   catch (ClassCastException e3) { Log.e(MainActivity.stProgramName, "ClassCastException on getting 'font_type'"); }
+   catch (ClassCastException e3) { Log.e(G.stProgramName, "ClassCastException on getting 'font_type'"); }
    try { fFontSize = Float.parseFloat(stFontSize); }
-   catch (NumberFormatException e4) { Log.e(MainActivity.stProgramName, "NumberFormatException on parsing 'font_size'"); }
-   Log.i(MainActivity.stProgramName, "Setting font size to "+fFontSize);
-   Log.i(MainActivity.stProgramName, "Setting font type to "+stFontType);
+   catch (NumberFormatException e4) { Log.e(G.stProgramName, "NumberFormatException on parsing 'font_size'"); }
+   Log.i(G.stProgramName, "Setting font size to "+fFontSize);
+   Log.i(G.stProgramName, "Setting font type to "+stFontType);
    
    // apply the settings
-   main.tabBeanshell_tvOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP, fFontSize);
-   main.tabTools_tvOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP, fFontSize);
+   G.tabBeanshell_tvOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP, fFontSize);
+   G.tabTools_tvOutput.setTextSize(TypedValue.COMPLEX_UNIT_SP, fFontSize);
    if (stFontType.equals("Monospace") )
    {
-     main.tabBeanshell_tvOutput.setTypeface(Typeface.MONOSPACE);
-     main.tabTools_tvOutput.setTypeface(Typeface.MONOSPACE);
+     G.tabBeanshell_tvOutput.setTypeface(Typeface.MONOSPACE);
+     G.tabTools_tvOutput.setTypeface(Typeface.MONOSPACE);
    }
    else
    {
-     main.tabBeanshell_tvOutput.setTypeface(Typeface.DEFAULT);
-     main.tabTools_tvOutput.setTypeface(Typeface.DEFAULT);
+     G.tabBeanshell_tvOutput.setTypeface(Typeface.DEFAULT);
+     G.tabTools_tvOutput.setTypeface(Typeface.DEFAULT);
    }
 } // fnApplySettings
 //===================================================================
 /** Set the defaults for the options
  */
-  private static void fnSetDefaults()
+  private void fnSetDefaults()
 //===================================================================
 {
   stFontSize="12";     // this must be a string representation of a float
--- src/com/t_arn/JavaIDEdroid/ToolTask.java
+++ src/com/t_arn/JavaIDEdroid/ToolTask.java
@@ -11,21 +11,19 @@ public class ToolTask
 //##################################################################
 { 
   private ProgressDialog progressDialog;
-  private MainActivity mainAct;
   private StringWriterOutputStream swos;
   
-public ToolTask (MainActivity main)
+public ToolTask ()
 //===================================================================
 {
-  this.mainAct = main;
 } //
 //===================================================================
 protected void onPreExecute() 
 //===================================================================
 {
-  Log.i(MainActivity.stProgramName, "onPreExecute");
+  Log.i(G.stProgramName, "onPreExecute");
   // show progress dialog
-  progressDialog = new ProgressDialog(mainAct);
+  progressDialog = new ProgressDialog(G.main);
   progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
   progressDialog.setTitle("Tool Task");
   progressDialog.setMessage("Task running...");
@@ -40,17 +38,17 @@ protected Void doInBackground(String... args)
   {
     String params = "";
     if (args.length>=2) params = args[1];
-    Log.i(MainActivity.stProgramName, "doInBackground");
+    Log.i(G.stProgramName, "doInBackground");
     swos = new StringWriterOutputStream();
-    mainAct.ide.fnRedirectOutput(swos);
-    if (args[0].equals("ECJ")) mainAct.ide.fnCompile(params);
-    else if (args[0].equals("DX")) mainAct.ide.fnDx(params);
-    else if (args[0].equals("ApkBuilder")) mainAct.ide.fnApkBuilder(params);
-    else if (args[0].equals("ZipSigner")) mainAct.ide.fnSignApk(params);
+    G.ide.fnRedirectOutput(swos);
+    if (args[0].equals("ECJ")) G.ide.fnCompile(params);
+    else if (args[0].equals("DX")) G.ide.fnDx(params);
+    else if (args[0].equals("ApkBuilder")) G.ide.fnApkBuilder(params);
+    else if (args[0].equals("ZipSigner")) G.ide.fnSignApk(params);
   }
   catch (Throwable t)
   {
-    mainAct.fnError("doInBackground", t);
+    G.fnError("doInBackground", t);
   }
   return null;
 }
@@ -59,16 +57,16 @@ protected void onProgressUpdate(String... progress)
 {
   try
   {
-    Log.i(MainActivity.stProgramName, "onProgressUpdate");
+    Log.i(G.stProgramName, "onProgressUpdate");
     progressDialog.setMessage(progress[0]);
-  } catch (Exception e) { Log.e(MainActivity.stProgramName, "onProgressUpdate: "+e.getMessage()); }
+  } catch (Exception e) { Log.e(G.stProgramName, "onProgressUpdate: "+e.getMessage()); }
 } // onProgressUpdate
 //===================================================================
 protected void onPostExecute(Void unused) 
 //===================================================================
 { 
   publishProgress("Task finished");
-  mainAct.tabTools_tvOutput.append(swos.toString());
+  G.tabTools_tvOutput.append(swos.toString());
   if (progressDialog.isShowing()) progressDialog.dismiss();
 } 
 //===================================================================
